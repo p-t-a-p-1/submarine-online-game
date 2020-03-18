fe6cddc53b5a193b7e1541d2e24234b5c4de124b
p-t-a-p-1 <kb240225+1@gmail.com>
2020-03-18 23:14:02 +0900

    [update] Twitter認証の設定、機能の完成

--- app.js
+++ app.js
@@ -6,9 +6,41 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var helmet = require('helmet')
+var session = require('express-session')
+var passport = require('passport')
+var Strategy = require('passport-twitter').Strategy
+var config = require('./config')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
+
+/**
+ * Twitterを利用した認証の戦略オブジェクトを設定
+ */
+passport.use(
+  new Strategy(
+    {
+      consumerKey: config.twitter.consumerKey,
+      consumerSecret: config.twitter.consumerSecret,
+      callbackURL: config.twitter.callbackURL
+    },
+    function(token, tokenSecret, profile, cb) {
+      process.nextTick(function() {
+        return cb(null, profile)
+      })
+    }
+  )
+)
+
+// ユーザーの情報をデータとして保存
+passport.serializeUser(function(user, cb) {
+  cb(null, user)
+})
+
+// 保存されたデータをユーザーの情報として呼び出す
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj)
+})
 
 var app = express()
 app.use(helmet())
@@ -23,8 +55,48 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+/**
+ * express-sessionとpassportでセッションの利用
+ */
+// セッションのオプション
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET, // セッションIDを利用する際の秘密鍵の文字列
+    resave: false, // セッションをストアに保存させない
+    saveUninitialized: false // セッションが初期化されていなくても保存しない
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+
+// Twitterに対して認証処理
+app.get('/login/twitter', passport.authenticate('twitter'))
+
+// Twitter認証のコールバック関数
+app.get(
+  '/oauth_callback', // Twitterが利用者の許可に対する問い合わせの結果を送るパス
+  passport.authenticate('twitter', { failureRedirect: '/' }),
+  function(req, res) {
+    // 認証成功時の処理
+    res.redirect('/')
+  }
+)
+
+// Twitter認証・ログアウト処理
+app.get('/logout', function(req, res) {
+  req.logout()
+  res.redirect('/')
+})
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/')
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  // ユーザーがTwitter認証を完了するとTwitterのアカウント情報がreq.userに入る
+  res.render('index', { title: '潜水艦ゲーム', user: req.user })
+})
 
-module.exports = router;
+module.exports = router
