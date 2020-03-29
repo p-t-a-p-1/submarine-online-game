62a837db1738eee27aee7f61dde9e50508a4fed9
p-t-a-p-1 <kb240225+1@gmail.com>
2020-03-29 22:53:50 +0900

    [update] レーダーの描画関数実装（33ミリ秒ごとに緑色の扇を回転しレーダーを描画）

--- app/entry.js
+++ app/entry.js
@@ -71,4 +71,30 @@ function drawRader(ctxRader) {
 
   // 現在のキャンバスの状態をセーブ
   ctxRader.save()
+
+  // 新しいレーダー描画開始
+  ctxRader.beginPath()
+  // レーダー画面の中央を(x, y)座標にする
+  ctxRader.translate(x, y)
+  // レーダーの座標をgameObj.deg度回転させる
+  ctxRader.rotate(getRadian(gameObj.deg))
+
+  // 描画するレーダーを半透明の緑色に設定
+  ctxRader.fillStyle = 'rgba(0, 220, 0, 0.5)'
+
+  /**
+   * 扇の弧の部分を原点(0, 0)を中心にして半径rで30度分描画する
+   * → 上で原点を(x, y)にしてるので、canvasの中心から半径rの円弧を30度だけ描画することになる
+   */
+  ctxRader.arc(0, 0, getRadian(0), getRadian(-30), true)
+  // 円弧を描いた筆で中心に向かって線を描画
+  ctxRader.lineTo(0, 0)
+
+  // 描画したエリアを塗りつぶす
+  ctxRader.fill()
+
+  // 元の設定を取得
+  ctxRader.restore()
+  // 角度を5度足す
+  gameObj.deg = (gameObj.deg + 5) % 360
 }
--- public/javascripts/index/bundle.js
+++ public/javascripts/index/bundle.js
@@ -85,15 +85,88 @@ var gameObj = {
   deg: 0,
   myDisplayName: (0, _jquery2.default)('#main').attr('data-displayName'),
   myThumbUrl: (0, _jquery2.default)('#main').attr('data-thumbUrl')
-};
 
-function init() {
+  /**
+   * 最初に実行する関数（ゲーム準備）
+   */
+};function init() {
   // ゲーム用のキャンバス
-  var raderCanvas = (0, _jquery2.default)('#rader');
-  console.log(raderCanvas);
-}
+  var raderCanvas = (0, _jquery2.default)('#rader')[0];
+  raderCanvas.width = gameObj.raderCanvasWidth;
+  raderCanvas.height = gameObj.raderCanvasHeight;
+  // canvasへの描画機能を有効化
+  gameObj.ctxRader = raderCanvas.getContext('2d');
 
+  // ランキング用のキャンバス
+  var scoreCanvas = (0, _jquery2.default)('#score')[0];
+  scoreCanvas.width = gameObj.scoreCanvasWidth;
+  scoreCanvas.height = gameObj.scoreCanvasHeight;
+  // canvasへの描画機能を有効化
+  gameObj.ctxScore = scoreCanvas.getContext('2d');
+
+  // 潜水艦の画像
+  var submarineImage = new Image();
+  submarineImage.src = '/images/submarine.png';
+  gameObj.submarineImage = submarineImage;
+}
 init();
+
+/**
+ * 時計の針のようにゲームの時を刻む
+ */
+function ticker() {
+  // canvas（#rader）の中身をまっさらにする
+  gameObj.ctxRader.clearRect(0, 0, gameObj.raderCanvasWidth, gameObj.raderCanvasHeight);
+  // レーダーを描画
+  drawRader(gameObj.ctxRader);
+  // 潜水艦を描画
+  drawSubmarine(gameObj.ctxRader);
+}
+// 33ミリ秒ごとに実行
+setInterval(ticker, 33);
+
+/**
+ * レーダーの描画処理
+ * ※レーダーは半透明の緑色の扇を回転するようにして表現
+ * @param {object} ctxRader レーダーの現在の状況
+ */
+function drawRader(ctxRader) {
+  // 中心座標のx軸
+  var x = gameObj.raderCanvasWidth / 2;
+  // 中心座標のy軸
+  var y = gameObj.raderCanvasHeight / 2;
+  // 半径 = 対角線の長さの半分とする
+  var r = gameObj.raderCanvasWidth * 1.5 / 2;
+
+  // 現在のキャンバスの状態をセーブ
+  ctxRader.save();
+
+  // 新しいレーダー描画開始
+  ctxRader.beginPath();
+  // レーダー画面の中央を(x, y)座標にする
+  ctxRader.translate(x, y);
+  // レーダーの座標をgameObj.deg度回転させる
+  ctxRader.rotate(getRadian(gameObj.deg));
+
+  // 描画するレーダーを半透明の緑色に設定
+  ctxRader.fillStyle = 'rgba(0, 220, 0, 0.5)';
+
+  /**
+   * 扇の弧の部分を原点(0, 0)を中心にして半径rで30度分描画する
+   * → 上で原点を(x, y)にしてるので、canvasの中心から半径rの円弧を30度だけ描画することになる
+   */
+  ctxRader.arc(0, 0, getRadian(0), getRadian(-30), true);
+  // 円弧を描いた筆で中心に向かって線を描画
+  ctxRader.lineTo(0, 0);
+
+  // 描画したエリアを塗りつぶす
+  ctxRader.fill();
+
+  // 元の設定を取得
+  ctxRader.restore();
+  // 角度を5度足す
+  gameObj.deg = (gameObj.deg + 5) % 360;
+}
 
 /***/ }),
 /* 1 */
